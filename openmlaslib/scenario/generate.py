e3242b270bb8a39b53b9f1e8bdc6828caa1e33e6
janvanrijn@gmail.com <janvanrijn@gmail.com>
2017-06-21 16:07:43 +0200

    updates

--- openmlaslib/scenario/generate.py
+++ openmlaslib/scenario/generate.py
@@ -6,10 +6,7 @@ from openmlaslib.utils.cvsconverter import generate_scenario
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--runtime", required=True, help="csv file with runtimes (rows instances, cols algorithms)")
-    parser.add_argument("--features", required=True, help="csv file with instance features (rows instances, cols features)")
-    parser.add_argument("--cutoff", required=True, type=float, help="runtime cutoff")
-
+    parser.add_argument("--study_id", required=True, help="openml study id")
     args_ = parser.parse_args()
 
-    generate_scenario(args_.runtime, args_.features, args_.cutoff)
+    generate_scenario(int(args_.study_id), "predictive_accuracy")
--- openmlaslib/utils/cvsconverter.py
+++ openmlaslib/utils/cvsconverter.py
@@ -7,20 +7,47 @@ Assumption: features with -512 (missing value) indicate a timeout during feature
 import arff
 import yaml
 import copy
+from openml.study import get_study
+from openml.evaluations import list_evaluations
 
+features = ["ClassEntropy", "DecisionStumpAUC", "DecisionStumpErrRate", "DecisionStumpKappa", "DefaultAccuracy", "Dimensionality", "EquivalentNumberOfAtts", "HoeffdingAdwin.changes", "HoeffdingAdwin.warnings", "HoeffdingDDM.changes", "HoeffdingDDM.warnings", "J48.00001.AUC", "J48.00001.ErrRate", "J48.00001.Kappa", "J48.0001.AUC", "J48.0001.ErrRate", "J48.0001.Kappa", "J48.001.AUC", "J48.001.ErrRate", "J48.001.Kappa", "MajorityClassPercentage", "MajorityClassSize", "MaxAttributeEntropy", "MaxKurtosisOfNumericAtts", "MaxMeansOfNumericAtts", "MaxMutualInformation", "MaxNominalAttDistinctValues", "MaxSkewnessOfNumericAtts", "MaxStdDevOfNumericAtts", "MeanAttributeEntropy", "MeanKurtosisOfNumericAtts", "MeanMeansOfNumericAtts", "MeanMutualInformation", "MeanNominalAttDistinctValues", "MeanSkewnessOfNumericAtts", "MeanStdDevOfNumericAtts", "MinAttributeEntropy", "MinKurtosisOfNumericAtts", "MinMeansOfNumericAtts", "MinMutualInformation", "MinNominalAttDistinctValues", "MinSkewnessOfNumericAtts", "MinStdDevOfNumericAtts", "MinorityClassPerentage", "MinorityClassSize", "NaiveBayesAUC", "NaiveBayesAdwin.changes", "NaiveBayesAdwin.warnings", "NaiveBayesDdm.changes", "NaiveBayesDdm.warnings", "NaiveBayesErrRate", "NaiveBayesKappa", "NoiseToSignalRatio", "NumberOfBinaryFeatures", "NumberOfClasses", "NumberOfFeatures", "NumberOfInstances", "NumberOfInstancesWithMissingValues", "NumberOfMissingValues", "NumberOfNumericFeatures", "NumberOfSymbolicFeatures", "PercentageOfBinaryFeatures", "PercentageOfInstancesWithMissingValues", "PercentageOfMissingValues", "PercentageOfNumericFeatures", "PercentageOfSymbolicFeatures", "Quartile1AttributeEntropy", "Quartile1KurtosisOfNumericAtts", "Quartile1MeansOfNumericAtts", "Quartile1MutualInformation", "Quartile1SkewnessOfNumericAtts", "Quartile1StdDevOfNumericAtts", "Quartile2AttributeEntropy", "Quartile2KurtosisOfNumericAtts", "Quartile2MeansOfNumericAtts", "Quartile2MutualInformation", "Quartile2SkewnessOfNumericAtts", "Quartile2StdDevOfNumericAtts", "Quartile3AttributeEntropy", "Quartile3KurtosisOfNumericAtts", "Quartile3MeansOfNumericAtts", "Quartile3MutualInformation", "Quartile3SkewnessOfNumericAtts", "Quartile3StdDevOfNumericAtts", "REPTreeDepth1AUC", "REPTreeDepth1ErrRate", "REPTreeDepth1Kappa", "REPTreeDepth2AUC", "REPTreeDepth2ErrRate", "REPTreeDepth2Kappa", "REPTreeDepth3AUC", "REPTreeDepth3ErrRate", "REPTreeDepth3Kappa", "RandomTreeDepth1AUC", "RandomTreeDepth1ErrRate", "RandomTreeDepth1Kappa", "RandomTreeDepth2AUC", "RandomTreeDepth2ErrRate", "RandomTreeDepth2Kappa", "RandomTreeDepth3AUC", "RandomTreeDepth3ErrRate", "RandomTreeDepth3Kappa", "StdvNominalAttDistinctValues", "kNN1NAUC", "kNN1NErrRate", "kNN1NKappa"]
 
-def generate_scenario(runtime_fn, features_fn, cutoff):
+def generate_scenario(study_id, measure):
     """ generates an ASlib scenario"""
 
-    description = {"scenario_id": "UNKNOWN",
+    study = get_study(study_id)
+    evaluations = list_evaluations("predictive_accuracy", setup=study.setups, task=study.tasks)
+
+    setup_flowname = {}
+    setup_scenarioname = {}
+    task_setup_result = {}
+    tasks = set()
+    setups = set()
+    for idx in range(len(evaluations)):
+        task_id = evaluations[idx].task_id
+        setup_id = evaluations[idx].setup_id
+        flowname = evaluations[idx].flow_name
+        value = evaluations[idx][value]
+
+        setup_flowname[setup_id] = flowname
+        setup_scenarioname[setup_id] = "%s_%s" %(setup_id,flowname)
+        tasks.add(task_id)
+        setups.add(setup_id)
+
+        if task_id not in task_setup_result:
+            task_setup_result[task_id] = {}
+        task_setup_result[task_id][setup_id] = value
+    algos = [setup_scenarioname[setup_id] for setup_id in setup_scenarioname.keys()]
+
+    description = {"scenario_id": "OpenML_study_%d" %study_id,
                    "performance_measures": ["runtime"],
-                   "maximize": [False],
-                   "performance_type": ["runtime"],
-                   "algorithm_cutoff_time": cutoff,
+                   "maximize": [True],
+                   "performance_type": [measure],
+                   "algorithm_cutoff_time": 0,
                    "algorithm_cutoff_memory": "?",
                    "features_cutoff_time": "?",
                    "features_cutoff_memory": "?",
-                   "algorithms_deterministic": "?",  # TODO
+                   "algorithms_deterministic": algos,
                    "algorithms_stochastic": "",
                    "features_deterministic": "?",  # TODO
                    "features_stochastic": "",
@@ -28,33 +55,30 @@ def generate_scenario(runtime_fn, features_fn, cutoff):
                    "feature_steps": {"ALL": {"provides": "?"}},  # TODO
                    "default_steps": ["ALL"]}
 
-    algos = []
-    with open(runtime_fn, "r") as fp:
-        algos = fp.readline().replace("\n", "").split(",")[1:]
-        algos = map(lambda x: x.strip("'").replace(" ", "_"), algos)
-        description["algorithms_deterministic"] = algos
-
-        attributes = [["instance_id", "STRING"],
+    run_attributes = [["instance_id", "STRING"],
                       ["repetition", "NUMERIC"],
                       ["algorithm", "STRING"],
-                      ["runtime", "NUMERIC"],
+                      [measure, "NUMERIC"],
                       ["runstatus", ["ok", "timeout", "memout", "not_applicable", "crash", "other"]]
-                      ]
+                     ]
 
-        data = []
-        for line in fp:
-            line = line.replace("\n", "").split(",")
-            inst = line[0]
-            for algo, perf in zip(algos, line[1:]):
-                status = "ok" if float(perf) < cutoff else "timeout"
-                data.append([inst, "1", algo, perf, status])
+    run_data = []
+    for task_id in tasks:
+        for setup_id in setups:
+            if setup_id in task_setup_result[task_id]:
+                perf = task_setup_result[task_id][setup_id]
+                status = "ok"
+            else:
+                perf = 0
+                status = "other"
+            run_data.append([task_id, "1", setup_scenarioname[setup_id], perf, status])
 
-    run_data = {"attributes": attributes,
-                "data": data,
+    run_arff = {"attributes": run_attributes,
+                "data": run_data,
                 "relation": "ALGORITHM_RUNS"
                 }
     with open("algorithm_runs.arff", "w") as fp:
-        arff.dump(run_data, fp)
+        arff.dump(run_arff, fp)
 
     instances = []
     status = {}
