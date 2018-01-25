80ce3dd471ab22e67b1bba192fd9bb2008b5bea0
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2018-01-25 15:22:58 +0100

    bugfix in joint table + added hyperparameters

--- openmlmetadata/export.py
+++ openmlmetadata/export.py
@@ -5,19 +5,50 @@ Authors: Jan van Rijn, Joaquin Vanschoren
 
 import arff
 import openml
+import pandas as pd
 from openml.study import get_study
 from openml.evaluations import list_evaluations
+
+def list_all(listing_call, *args, **filters):
+    """Helper to handle paged listing requests.
+    Example usage: evaluations = list_all(list_evaluations, "predictive_accuracy", task=mytask)
+
+    Parameters
+    ----------
+    listing_call : object
+        Name of the listing call, e.g. list_evaluations
+    *args : Variable length argument list
+        Any required arguments for the listing call
+    **filters : Arbitrary keyword arguments
+        Any filters that need to be applied
+
+    Returns
+    -------
+    object
+    """
+    batch_size = 10000
+    page = 0
+    has_more = 1
+    result = {}
+    while has_more:
+        new_batch = listing_call(*args, size=batch_size, offset=batch_size*page, **filters)
+        result.update(new_batch)
+        page += 1
+        has_more = (len(new_batch) == batch_size)
+    return result
+
 
 def generate_files(study_id, measure):
 
     # Fetch all its evaluations for a specific study
     print("Fetching evaluation results from OpenML...")
     study = get_study(study_id)
-    evaluations = list_evaluations(measure, setup=study.setups, task=study.tasks)
+    evaluations = list_all(list_evaluations, measure, setup=study.setups, task=study.tasks)
 
     setup_flowid = {}
     task_data_id = {}
     setup_name = {}
+    setup_detail = {}
     task_setup_result = {}
     task_qualities = {}
     tasks = set()
@@ -45,18 +76,28 @@ def generate_files(study_id, measure):
     # obtain the meta-features
     complete_quality_set = None
     for task_id in tasks:
-        qualities = openml.datasets.get_dataset(task_data_id[task_id]).qualities
-        task_qualities[task_id] = qualities
-        if complete_quality_set is None:
-            complete_quality_set = qualities.keys()
-        else:
-            complete_quality_set = complete_quality_set & qualities.keys()
+        try:
+            qualities = openml.datasets.get_dataset(task_data_id[task_id]).qualities
+            task_qualities[task_id] = qualities
+            if complete_quality_set is None:
+                complete_quality_set = qualities.keys()
+            else:
+                complete_quality_set = complete_quality_set & qualities.keys()
+        except arff.BadDataFormat:
+            print("Error parsing dataset: "+str(task_data_id[task_id]))
+        except arff.BadAttributeType:
+            print("Error parsing dataset: "+str(task_data_id[task_id]))
     complete_quality_set = list(complete_quality_set)
 
     print("Exporting evaluations...")
     for setup_id in setups:
         flow = openml.flows.get_flow(setup_flowid[setup_id])
         setup_name[setup_id] = "%s_%s" % (setup_id, flow.name)
+        setup_params = openml.setups.get_setup(setup_id).parameters
+        if setup_params:
+            setup_detail[setup_id] = ','.join("{}:{}".format(p.parameter_name, p.value) for p in setup_params.values())
+        else:
+            setup_detail[setup_id] = ''
 
     run_data = []
     for task_id in tasks:
@@ -67,11 +108,12 @@ def generate_files(study_id, measure):
             else:
                 perf = 0
                 status = "other"
-            run_data.append([task_id, "1", setup_name[setup_id], perf, status])
+            run_data.append([task_id, "1", setup_name[setup_id], setup_detail[setup_id]+" ", perf, status])
 
-    run_attributes = [["openml_data_id", "STRING"],
+    run_attributes = [["openml_task_id", "NUMERIC"],
                       ["repetition", "NUMERIC"],
                       ["algorithm", "STRING"],
+                      ["hyperparameters", "STRING"],
                       [measure, "NUMERIC"],
                       ["runstatus", ["ok", "timeout", "memout", "not_applicable", "crash", "other"]]
                      ]
@@ -83,7 +125,7 @@ def generate_files(study_id, measure):
         arff.dump(run_arff, fp)
 
     print("Exporting meta-features...")
-    qualities_attributes = [["openml_data_id", "STRING"],
+    qualities_attributes = [["openml_task_id", "NUMERIC"],
                             ["repetition", "NUMERIC"]]
     for f in complete_quality_set:
         qualities_attributes.append([f, "NUMERIC"])
@@ -101,3 +143,25 @@ def generate_files(study_id, measure):
                      }
     with open("output/study_" + str(study_id) + "_metafeatures.arff", "w") as fp:
         arff.dump(qualities_arff, fp)
+
+    print("Exporting joint table...")
+    eval_labels = ['openml_task_id', 'repetition', 'algorithm', 'hyperparameters', measure, 'runstatus']
+    df_evals = pd.DataFrame.from_records(run_data, columns=eval_labels)
+    quality_labels = ['openml_task_id', 'repetition']
+    quality_labels.extend(complete_quality_set);
+
+    df_qualities = pd.DataFrame.from_records(qualities_data, columns=quality_labels)
+
+    joint_data = pd.merge(df_evals, df_qualities, how='left', on=['openml_task_id','repetition'])
+
+    joint_attributes = run_attributes
+    for f in complete_quality_set:
+        joint_attributes.append([f, "NUMERIC"])
+
+    joint_arff = {"attributes": joint_attributes,
+                  "data": joint_data.values,
+                  "relation": "JOINTMETADATA"
+                 }
+
+    with open("output/study_" + str(study_id) + "_joint.arff", "w") as fp:
+        arff.dump(joint_arff, fp)
